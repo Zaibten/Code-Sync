c0cff7633a2ab7471bdbed2f493514e795c2ca45
Muzamil Khan <113015136+Zaibten@users.noreply.github.com>
2026-01-08 16:30:13 +0500

    Update global_variables.dart

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 //String uri = 'http://192.168.0.106:7000';
-String uri = 'http://192.168.100.39:9000';
+String uri = 'https://code-sync-server-kappa.vercel.app';
 
 class GlobalVariables {
   // COLORS
